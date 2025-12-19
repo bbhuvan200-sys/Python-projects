8bfddc91ae2c3b6a55e7ac20668a452bf9ab15d6
bbhuvan200-sys <bbhuvan200@gmail.com>
2025-12-19 14:07:46 +0530

    Add files via upload

--- Email Validation/email_validate.py
+++ Email Validation/email_validate.py
@@ -1,1 +1,14 @@
+# a-z demoss@gmail.compile
+# 0-9
+# ._time 1
+# @ time 1
+# .2,3
+import re
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$" 
+user_email=input(' enter your mail: ')            
+
+if re.search(email_condition,user_email):
+    print(" right email")
+else:
+    print("wrong email")                 
 
